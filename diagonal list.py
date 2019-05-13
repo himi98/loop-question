e978d4cdc3065c1c7cc3bf969a14040b2c35d5ff
priyapriyam <priyam18@navgurukul.org>
2019-05-13 15:04:55 +0530

    my first commit

---
+++ diagonal list.py
@@ -0,0 +1,14 @@
+list=[[5,6,8],[9,2,4],[6,7,9]]
+sum=0
+i=0
+while i<len(list):
+    sum=sum+list[i][i]
+    i=i+1
+print sum
+list=[7,8,9,6,4]#(reverse number)
+j=-1
+while j<len(list):
+    print list[j]
+    j=j-1
+
+    
---
+++ number1.py
@@ -0,0 +1,10 @@
+row=1
+num=0
+while row <=4:
+	column=0
+	while column<row:
+		print num,
+		column=column+1
+		num=num+1
+	print""
+	row=row+1
